20db41a5b95d902ceed8526c1e55d7e4f58c1a5c
Uli Kusterer <ulivc@zathras.de>
2016-04-30 20:55:10 +0200

    Initial Commit

---
+++ TableView/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  TableView
+//
+//  Created by Uli Kusterer on 30/04/16.
+//  Copyright © 2016 Uli Kusterer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
