8a1f377aad3591dbf6271dcd83776f128bb9ad34
Uli Kusterer <ulivc@zathras.de>
2016-04-30 22:01:09 +0200

    Working table view still uses too much code.

--- TableView/AppDelegate.swift
+++ TableView/AppDelegate.swift
@@ -8,20 +8,46 @@
 
 import Cocoa
 
+
+public class Row: NSObject {
+    var name: String = ""
+    var explanation: String = ""
+    var image: NSImage?
+    
+    init( name inName: String, explanation inExplanation: String, image inImageName: String ) {
+        name = inName
+        explanation = inExplanation
+        image = NSImage(named: inImageName)
+    }
+}
+
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+public class AppDelegate: NSObject, NSApplicationDelegate, NSTableViewDelegate, NSTableViewDataSource {
 
-    @IBOutlet weak var window: NSWindow!
-
-
-    func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+    @IBOutlet var rowsTable: NSTableView!
+    @IBOutlet var window: NSWindow!
+    
+    var rows: [Row] = []
+    
+    @IBAction func addNewRow(sender: AnyObject) {
+        rows.append( Row( name: "New", explanation: "New Ex", image: NSImageNameColorPanel) )
+        rowsTable.reloadData()
     }
 
-    func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
+    public func numberOfRowsInTableView(tableView: NSTableView) -> Int {
+        return rows.count
+    }
+    
+    public func tableView(tableView: NSTableView, objectValueForTableColumn tableColumn: NSTableColumn?, row: Int) -> AnyObject? {
+        return rows[row]
     }
 
-
+    public func applicationDidFinishLaunching(aNotification: NSNotification) {
+        rows = [ Row( name: "User", explanation: "One Ex", image: NSImageNameUser),
+                 Row( name: "Folder", explanation: "Two Ex", image: NSImageNameFolder),
+                 Row( name: "Bonjour", explanation: "Three Ex", image: NSImageNameBonjour)
+                ]
+        rowsTable.reloadData()
+    }
 }
 
